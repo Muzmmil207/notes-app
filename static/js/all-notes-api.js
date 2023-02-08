85a82af551e44aa447ea8ace77c49819e1d53328
mozmmil-ali <mly88207@gmail.com>
2023-02-08 21:38:13 +0200

    setup the frontend template

--- static/js/all-notes-api.js
+++ static/js/all-notes-api.js
@@ -1,14 +1,30 @@
-let notesContainer = document.querySelector('.all-notes')
-
+let category = document.getElementById('category')
+let date = document.getElementById('date')
+let title = document.getElementById('title')
+let content = document.getElementById('content')
+let notesContainer = document.getElementById('notes')
 let url = '/api/all-notes'
-fetch(url)
-    .then(response => response.json())
-    .then(function (data) {
-        console.log(data)
-        let notes = data.results
-        for (let i = 0; i < notes.length; i++) {
+async function GetNotes() {
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data)
+    let notes = data.results
+    for (let i = 0; i < notes.length; i++) {
 
-            notesContainer.innerHTML += `<a href="${notes[i]['id']}">${notes[i].title}</a><hr>`
-        }
-    })
-
+        notesContainer.innerHTML += `
+            <div class="note">
+            <div class="head">
+                <div class="category" id="category">${notes[i].category || ''} </div>
+                <div class="date" id="date">${notes[i].updated_at}</div>
+            </div>
+            <div class="content">
+                <a href="${notes[i]['id']}" class="title" id="title">${notes[i].title || ''}</a>
+                <a href="${notes[i]['id']}" class="note-text" id="content">
+                    ${notes[i].content || ''}
+                </a>
+            </div>
+        </div>
+            `
+    }
+}
+GetNotes()
